e8fcc714348d1ba59cd70f62f5a74f1946eff1c8
Giancarlo Rosero <pgiancarlo@uninorte.edu.co>
2022-08-02 23:34:03 -0500

    Inicio de servicios

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, Response
+from flask.json import jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('base.html')
+@app.route('/login', methods=['GET'])
+def login():
+    #email = request.json['email']
+
+    return {'message': 'Bien'}
