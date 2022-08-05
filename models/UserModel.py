b33ae8becc3aaf53292a122b7dcb45ed47ba4997
Giancarlo Rosero <pgiancarlo@uninorte.edu.co>
2022-08-05 10:27:54 -0500

    ajuste servicios

--- models/UserModel.py
+++ models/UserModel.py
@@ -13,12 +13,12 @@ class UserModel():
 
             with connection.cursor() as cursor:
                 cursor.execute(
-                    """SELECT password FROM public.user where email = %s """, (email,))
+                    """SELECT password, id, rol_id FROM public.user where email = %s """, (email,))
                 result = cursor.fetchone()
 
                 response = jsonify(statusCode=401, message='Login failed, credentials incorrect'), 401
                 if result != None and check_password_hash(result[0], password):
-                    response = jsonify(statusCode=200, message='Login success'), 200
+                    response = jsonify(statusCode=200, message='Login success', id=result[1], rol=result[2]), 200
 
             connection.close()
             return response
